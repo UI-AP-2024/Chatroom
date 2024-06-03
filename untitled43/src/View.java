159ca64d45b9ddc90498a9b0728fa9536e189f29
Fatemeh614 <Fatemehjafarzadeh614@gmail.com>
2024-06-03 14:28:54 +0430

    client2

---
+++ untitled43/src/View.java
@@ -0,0 +1,50 @@
+
+
+import java.io.PrintWriter;
+import java.util.Scanner;
+
+public class View
+{
+    private static View view;
+    private View(){}
+
+    public static View getView() {
+        if(view==null)
+            view=new View();
+        return view;
+    }
+    public void start()
+    {
+        try(PrintWriter writer=new PrintWriter(Client.getSocket().getOutputStream());)
+        {
+            Scanner sc=new Scanner(System.in);
+            writer.write(Client.getID()+"\n");
+            writer.write(Client.getName()+"\n");
+            writer.flush();
+            while (true)
+            {
+                String input=sc.nextLine();
+                if(input.compareTo("exit")==0)
+                {
+                    writer.write("exit\n");
+                    Client.getSocket().close();
+                    break;
+                }
+                else if(input.compareTo("pv")==0)
+                    ;
+                else if(input.compareTo("search")==0)
+                    ;
+                else if(input.compareTo("ping")==0)
+                    ;
+                else
+                {
+                    writer.write(input+"\n");
+                    writer.flush();
+                }
+            }
+        }catch (Exception exception)
+        {
+            System.out.println("error "+exception.getMessage());
+        }
+    }
+}
