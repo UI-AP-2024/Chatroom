de89b39a9a1e9824053ec69ffd5e778c8ea6cdf6
mohsen <im.mohsen.369@gmail.com>
2024-06-04 20:34:05 +0430

    create a simple chatroom

--- Client/src/main/java/org/example/client/HelloApplication.java
+++ Client/src/main/java/org/example/client/HelloApplication.java
@@ -12,8 +12,8 @@ import java.sql.SQLException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("main-page-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 500, 240);
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("chatroom-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 600, 400);
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
