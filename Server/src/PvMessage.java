575a3db4dce3e53a0a22ef86a5f044a6464e2f5e
mohsen <im.mohsen.369@gmail.com>
2024-06-07 17:55:35 +0430

    Creating PvMessage class

---
+++ Server/src/PvMessage.java
@@ -0,0 +1,14 @@
+import lombok.Getter;
+import lombok.Setter;
+
+import java.text.ParseException;
+
+@Getter
+@Setter
+public class PvMessage extends Message{
+    private int sentToID;
+    private PvMessage(String content, int sentByIDm, int sentToID) throws ParseException {
+        super(content, sentByIDm);
+        this.sentToID = sentToID;
+    }
+}
